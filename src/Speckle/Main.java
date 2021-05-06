e6bf37339c9ac8b6a1b460e499135f4bbc70adb0
XaHertz <xahertz@gmail.com>
2021-05-06 17:58:14 +0530

    LAF Changed from Nimbus to FlatLaf

--- src/Speckle/Main.java
+++ src/Speckle/Main.java
@@ -1,5 +1,7 @@
 package Speckle;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 /**
  *
  * @author Astralsphere Arts
@@ -143,14 +145,13 @@ public class Main extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+        /* Set the FlatDarkLaf look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        /* If FlatDarkLaf is not available, stay with the default look and feel. */
+        FlatDarkLaf.install();
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("FlatDarkLaf".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
