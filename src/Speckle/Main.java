2f191ff5768a88954cca170467b99ca27c1d7df0
akshat1222 <akshat.singh786p@gmail.com>
2021-07-05 18:15:41 +0530

    Added Security in signin and signup
    implemented cipher in signin and signup
    in main form

--- src/Speckle/Main.java
+++ src/Speckle/Main.java
@@ -758,8 +758,10 @@ public class Main extends javax.swing.JFrame {
         String passwd = new String(Password.getPassword());
         String usname = getEncodedString(usrname);
         String pswd = getEncodedString(passwd);
-        
-       if (Internal.SQLite.logIn(usname, pswd)) {
+        if (usrname.equals("") && passwd.equals(""))
+            JOptionPane.showMessageDialog(null, "Please enter Username and Password they cannot be"
+                + " Blank. Please Try Again!", "Credentials are Blank", JOptionPane.ERROR_MESSAGE);
+        else if (Internal.SQLite.logIn(usname, pswd)) {
             Content.removeAll();
             Speckle.Home scene = new Home();
             scene.setBounds(0, 0, 948, 574);
@@ -771,14 +773,11 @@ public class Main extends javax.swing.JFrame {
                 + " Please Try Again!", "Incorrect Credentials", JOptionPane.ERROR_MESSAGE);
     }//GEN-LAST:event_SignIn_ButtonActionPerformed
     
-    private static String getEncodedString(String decryptedString) {
-       return Base64.getEncoder().encodeToString(decryptedString.getBytes());
-    }
-    
     private void Next_ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Next_ButtonActionPerformed
         String usrname = SignUp_Username.getText();
         String passwd = new String(Create_Password.getPassword());
         String confpasswd = new String(Confirm_Password.getPassword());
+        
         if (usrname.equals("") || passwd.equals(""))
             JOptionPane.showMessageDialog(null, "Username or Password cannot be Empty. Please"
                 + " Try Again!", "Username/Password Empty", JOptionPane.ERROR_MESSAGE);
@@ -786,11 +785,17 @@ public class Main extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "The Passwords in Password Fields do not Match."
                 + " Please Try Again!", "Password Mismatch", JOptionPane.ERROR_MESSAGE);
         else {
-            Internal.SQLite.userConfig(usrname, passwd);
+            String usname = getEncodedString(usrname);
+            String pswd = getEncodedString(passwd);
+            Internal.SQLite.userConfig(usname, pswd);
             StartUp_Deck.show(StartUp_Container,"signUp2");
         }
     }//GEN-LAST:event_Next_ButtonActionPerformed
 
+     private static String getEncodedString(String decryptedString) {
+       return Base64.getEncoder().encodeToString(decryptedString.getBytes());
+    }
+     
     private void SignUp_ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SignUp_ButtonActionPerformed
         String cname = Business_Name.getText();
         String cnum = Contact_Number.getText();
