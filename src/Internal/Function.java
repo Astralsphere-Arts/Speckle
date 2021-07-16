85e5a9f6218541049600708bce9a7735ed0e05d3
akshat1222 <akshat.singh786p@gmail.com>
2021-07-16 22:33:29 +0530

    Update Function.java
    Added randomAlphaNumeric method which generates recovery key for password recovery

--- src/Internal/Function.java
+++ src/Internal/Function.java
@@ -44,6 +44,16 @@ public class Function {
         return builder.toString();
     }
     
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+       public static String randomAlphaNumeric(int count) {
+       StringBuilder builder = new StringBuilder();
+       while (count-- != 0) {
+       int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+       builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+      }
+        return builder.toString();
+    }
+    
     public static TableModel invoTableModel() {
         ResultSet invResult = Internal.SQLite.invoData();
         DefaultTableModel invTableModel = new DefaultTableModel() {
